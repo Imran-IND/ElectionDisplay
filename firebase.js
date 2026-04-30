4ebe79a4b9d7157c0916b8e980173708dc1e2134
Imran-IND <ihussainmuhammad9@gmail.com>
2026-04-30 20:12:45 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,17 @@
-// Your Firebase configuration
+// Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyAtncVSR1m9lkxLdfCzWKWfa7KCsvKzXR4",
   authDomain: "electiondisplay-cd9a1.firebaseapp.com",
   projectId: "electiondisplay-cd9a1",
-  storageBucket: "electiondisplay-cd9a1.firebasestorage.app",
   messagingSenderId: "653096453446",
   appId: "1:653096453446:web:d1cd5e3db24153a4739147"
 };
 
+// Init
 firebase.initializeApp(firebaseConfig);
 
+// ✅ ONLY Firestore (NO STORAGE)
 const db = firebase.firestore();
-const storage = firebase.storage();
-
-
 window.db = db;
-window.storage = storage;
 
 console.log("Firebase Connected");
